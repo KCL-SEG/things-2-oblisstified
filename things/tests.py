42414e396066596a8ef6b1af24704ad660fbad3c
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-10-28 13:06:27 +0100

    first commit

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django import forms
+from things.forms import ThingForm
 
-# Create your tests here.
+class ThingFormTestCase(TestCase):
+    def setUp(self):
+        self.form_input = {
+        'name' : 'aleks',
+        'description': 'i dont know what im actually doing',
+        'quantity' : 1
+        }
+
+    def test_valid_user(self):
+        form = ThingForm(data = self.form_input)
+        self.assertTrue(form.is_valid())
